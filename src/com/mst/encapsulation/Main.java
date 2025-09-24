801e904073858fae8d95f0569464558facc587dc
oRABiiA <Rabealahham2@gmail.com>
2025-09-24 20:13:01 +0300

    Student Class

---
+++ src/com/mst/encapsulation/Main.java
@@ -0,0 +1,7 @@
+package com.mst.encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/com/mst/encapsulation/Student.java
@@ -0,0 +1,38 @@
+package com.mst.encapsulation;
+
+public class Student {
+    int studentId;
+    String name;
+    double grade;
+
+    public Student(){}
+
+    public Student(int studentId, String name, double grade) {
+        this.studentId = studentId;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public void setStudentId(int id){
+        this.studentId = id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setGrade(double grade){
+        if(grade < 0 || grade > 100){
+            return;
+        }
+        this.grade = grade;
+    }
+
+    public int getStudentId() {return studentId;}
+    public String getName() {return name;}
+    public double getGrade() {return grade;}
+
+    public boolean isPassing(){
+        return grade >= 60;
+    }
+}
